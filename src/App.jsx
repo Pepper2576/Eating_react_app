1d39534debe94bf3fb4a656169f3e67200e6d23f
Pepper2576 <107074452+Pepper2576@users.noreply.github.com>
2023-03-06 15:19:42 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+	// app components
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>Test</h1>
+			</header>
+		</div>
+	);
+}
+
+export default App;
