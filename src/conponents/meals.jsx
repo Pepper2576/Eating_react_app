db3e01c24ca3dae714de77c530fdbf24a8e2ca1a
Pepper2576 <107074452+Pepper2576@users.noreply.github.com>
2023-03-07 21:30:53 +0000

    begun working on css

--- src/conponents/meals.jsx
+++ src/conponents/meals.jsx
@@ -5,12 +5,29 @@ const Meals = () => {
 	const { meals } = useGlobalContext();
 
 	return (
-		<section>
+		<section className='section-center'>
 			{/* Mapps the meal array */}
 			{meals.map((singleMeal) => {
-				console.log(singleMeal);
+				// assining elements of the array uneque values
+				const { idMeal, strMeal: title, strMealThumb: image } = singleMeal;
 				// display element of the array
-				return <h4>{singleMeal.strMeal}</h4>;
+				return (
+					<div
+						key={idMeal}
+						className='single-meal'
+					>
+						<img
+							src={image}
+							alt='Food'
+							style={{ width: "200px" }}
+							className='img'
+						/>
+						<footer>
+							<h5>{title}</h5>
+							<button className='like-btn'>click me</button>
+						</footer>
+					</div>
+				);
 			})}
 		</section>
 	);
